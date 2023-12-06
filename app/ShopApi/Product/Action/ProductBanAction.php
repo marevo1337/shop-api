d46cdf7e0407324bd8f323c92909b1b41618ca27
Artem Tiutnev <artem.tiutnev@gmail.com>
2023-12-06 20:09:23 +0800

    Рефакторинг

--- app/ShopApi/Product/Action/ProductBanAction.php
+++ app/ShopApi/Product/Action/ProductBanAction.php
@@ -6,11 +6,15 @@ use App\ShopApi\Exception\RuntimeException;
 use App\ShopApi\Product\Contract\ProductIdentificationDataInterface;
 use App\ShopApi\Product\Enum\ProductStatusEnum;
 use App\ShopApi\Product\Storage\ProductStorageInterface;
+use App\ShopApi\Security\Permission\Service\PermissionChecker;
 
 class ProductBanAction
 {
+    private const ACTION = 'ProductBan';
+
     public function __construct(
-        private readonly ProductStorageInterface $productStorage
+        private readonly ProductStorageInterface $productStorage,
+        private readonly PermissionChecker       $permissionChecker
     ) {}
 
     /**
@@ -18,6 +22,8 @@ class ProductBanAction
      */
     public function run(ProductIdentificationDataInterface $data): void
     {
+        $this->permissionChecker->check(self::ACTION);
+
         $product = $this->productStorage->findByAlias($data->getAlias());
         if (is_null($product)) {
             throw new RuntimeException('Товар не найден');
